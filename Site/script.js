dd77d4378ef2e692091812a2374db6ef507939fb
Shrey Shah <fivenightsatbothra@gmail.com>
2026-03-25 19:34:29 +0530

    feat: added snappy cubic-bezier UI animations, stagger effects, and raining SVGs 404 page

--- Site/script.js
+++ Site/script.js
@@ -360,7 +360,7 @@ function renderIconsGrid(collection, files) {
         if (chunk.length === 0) return;
 
         const html = chunk.map((file, idx) => `
-            <div class="icon-item" data-id="search-icon-${file.name}" data-filename="${file.name}" data-collection="${file.customCollection || collection.name}" data-author="${file.customAuthor || collection.author}">
+            <div class="icon-item" data-id="search-icon-${file.name}" data-filename="${file.name}" data-collection="${file.customCollection || collection.name}" data-author="${file.customAuthor || collection.author}" style="animation-delay: ${(idx % 50) * 15}ms">
                 ${file.content}
                 <div class="icon-quick-actions">
                     <button title="Copy SVG" class="quick-copy-svg"><svg viewBox="0 0 24 24" width="12" height="12" stroke="currentColor" stroke-width="2" fill="none"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg> copy</button>
